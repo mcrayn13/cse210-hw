dfc60c223d273a68dfc4b443dd6416dba54facee
mcrayn13 <mcrayn13@gmail.com>
2025-01-12 13:42:36 +0800

    prep 1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,5 +5,12 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+         
+         Console.Write("What is Your First name?");
+        string first= Console.ReadLine ();
+        Console.Write ("What is Your Last name?");
+        string last= Console.ReadLine ();
+        Console.WriteLine ($"Your name is {last}, {first} {last}.");
+
     }
 }
